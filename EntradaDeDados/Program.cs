21eeaea9adfd794b5d2473f5dfd7261c85bf60f7
bmotadev <bmotadev@gmail.com>
2022-10-03 10:58:28 -0300

    utilizando split

--- EntradaDeDados/Program.cs
+++ EntradaDeDados/Program.cs
@@ -7,8 +7,30 @@ namespace Course
         static void Main(string[] args)
         {
             string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
 
-            Console.WriteLine("Você digitou: " + frase);
+            
+
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            #region Valores do Vetor
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            #endregion
+
+
         }
     }
 }
