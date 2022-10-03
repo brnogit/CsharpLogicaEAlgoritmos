82bab796dba9a5e8ea316d1b8ced018dc1633d40
bmotadev <bmotadev@gmail.com>
2022-10-03 10:38:37 -0300

    processamento de dados e casting

---
+++ ProcessamentoDeDadosExemplo5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            int b;
+
+            a = 5.0;
+            b = (int) a;
+
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
