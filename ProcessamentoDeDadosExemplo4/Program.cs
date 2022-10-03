aa752608e332ecccc24d2aa9e5f5a208e6b8b3a3
bmotadev <bmotadev@gmail.com>
2022-10-03 10:30:43 -0300

    processamento de dados exemplo 4

---
+++ ProcessamentoDeDadosExemplo4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+
+            resultado = (double) a / b;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
