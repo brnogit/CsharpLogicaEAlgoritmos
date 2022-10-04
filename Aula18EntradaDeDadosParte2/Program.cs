50e60478b63c634dbdbcc712c03c700d6c810afc
bmotadev <bmotadev@gmail.com>
2022-10-04 12:34:03 -0300

    aula 18 - utilizando o cultureinfo

--- Aula18EntradaDeDadosParte2/Program.cs
+++ Aula18EntradaDeDadosParte2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -7,9 +8,22 @@ namespace Course
         static void Main(string[] args)
         {
             int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
             Console.WriteLine("Você digitou: ");
             Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
