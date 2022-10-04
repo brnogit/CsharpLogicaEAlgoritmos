fcbe93ab7ac5d0f71e210bb34e176d3ab3488bae
bmotadev <bmotadev@gmail.com>
2022-10-04 12:20:33 -0300

    aula 18 - entrada de dados parte 2

---
+++ Aula18EntradaDeDadosParte2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(n1);
+        }
+    }
+}
+
