db137a190a1cac5ac22f0bb82c5c3131a7eb2d8b
bmotadev <bmotadev@gmail.com>
2022-10-05 16:50:37 -0300

    Primeiro Exercicio no URI

--- Aula19FuncoesMatematicas/Program.cs
+++ Aula19FuncoesMatematicas/Program.cs
@@ -12,21 +12,21 @@ namespace curso
             double z = -5.0;
             double A, B, C;
 
-            A = Math.Sqrt(x);
+            A = Math.Sqrt(x); // Variável A recebe a raiz quadrada de x
             B = Math.Sqrt(y);
             C = Math.Sqrt(25.0);
             Console.WriteLine("Raiz quadrada de " + x + " = " + A);
             Console.WriteLine("Raiz quadrada de " + y + " = " + B);
             Console.WriteLine("Raiz qudrada de 25 = " + C);
 
-            A = Math.Pow(x, y);
+            A = Math.Pow(x, y); // Variável A recebe o resultado de x elevado a y
             B = Math.Pow(x, 2.0);
             C = Math.Pow(5.0, 2.0);
             Console.WriteLine(x + " elevado a " + y + " = " + A);
             Console.WriteLine(x + " elevado ao quadrado = " + B);
             Console.WriteLine("5 elevado ao quadrado = " + C);
 
-            A = Math.Abs(y);
+            A = Math.Abs(y); // Variável A recebe o valor absoluto de x
             B = Math.Abs(z);
             Console.WriteLine("Valor absoluto de " + y + " = " + A);
             Console.WriteLine("Valor absoluto de " + z + " = " + B);
