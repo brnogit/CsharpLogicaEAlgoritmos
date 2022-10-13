51ea0d18ca5fc5dd204f0696483063baa036c359
bmotadev <bmotadev@gmail.com>
2022-10-13 15:52:13 -0300

    Aula 32 Estrutura Condicional Composta

---
+++ Aula32EstruturaCondicionalComposta/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace estruturacondicionalcomposta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+
+            Console.Write("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
