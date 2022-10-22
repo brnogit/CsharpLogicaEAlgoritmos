0c644f1c9fdd59943d7d3f4a5c9ae2860da05e93
bmotadev <bmotadev@gmail.com>
2022-10-22 15:31:20 -0300

    aula 46 estrutura repetitiva para for

---
+++ Aula46EstruturaRepetitivaPara/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, soma;
+
+            n = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
